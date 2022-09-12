937dcae95434f636000a796f3229e4455ecc13e5
yvette <vette.rosario@gmail.com>
2022-09-12 16:17:06 -0400

    feature 5 complete

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+USER_MODEL = settings.AUTH_USER_MODEL
+
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    members = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="projects"
-    )
+    members = models.ManyToManyField(USER_MODEL, related_name="projects")
 
     def __str__(self):
         return self.name
